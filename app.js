1fa398921bbb4482ca6ae49f76bc481198be1057
sangbae.Yun <yundream@gmail.com>
2023-11-01 19:57:29 +0900

    express static web example

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const logger = require('./logger')
 const app = express()
 const port = 8080
 
-app.use(express.static('webpage'))
+
 app.use('/static', express.static('public'))
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+"webpage/index.html")
+    res.sendFile(__dirname+"/webpage/index.html")
+    logger.info({user_id: 'yundream', transaction_id: '1234567'}, "Homepage loaded")
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
